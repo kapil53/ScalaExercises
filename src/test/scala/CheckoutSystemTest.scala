86b24030435397ee2d392751139f2f701562b750
kapil sanghvi <kapil.sanghvi@anatwine.com>
2018-05-08 15:14:14 +0100

    Step 2 implementation with unit test

--- src/test/scala/CheckoutSystemTest.scala
+++ src/test/scala/CheckoutSystemTest.scala
@@ -7,7 +7,11 @@ class CheckoutSystemTest extends FlatSpec {
     """
       |# Checkout System
       |
+      |##`calculateTotalCost`
       |It takes a list of items scanned at the till and outputs the total cost
+      |
+      |##`calculateTotalCostAfterOffer`
+      |It takes a list of items scanned at the till and outputs the total cost after applying the offer on items
       |
     """.stripMargin
   }
@@ -18,6 +22,18 @@ class CheckoutSystemTest extends FlatSpec {
     val scannedItems = List(Apple, Apple, Orange, Apple)
 
     CheckoutSystem.calculateTotalCost(scannedItems) should be("£2.05")
+  }
+
+  behavior of "calculateTotalCostAfterOffer"
+
+  it should "calculate the total cost for scanned items with offer applied" in {
+    val scannedItems = List(Apple, Apple, Orange, Apple, Apple, Apple)
+
+    CheckoutSystem.calculateTotalCostAfterOffer(scannedItems) should be("£2.05")
+
+    val anotherListOfItems = List(Apple, Apple, Orange, Orange, Orange, Orange, Apple)
+
+    CheckoutSystem.calculateTotalCostAfterOffer(anotherListOfItems) should be("£1.95")
   }
 
 }
