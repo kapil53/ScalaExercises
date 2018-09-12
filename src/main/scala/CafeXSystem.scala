760b75bc2d3b7f1b92de05b290cd0a41a625852e
kapil sanghvi <kapil.sanghvi@anatwine.com>
2018-09-12 22:38:03 +0100

    Added implementation for Step2 - including unit tests

--- src/main/scala/CafeXSystem.scala
+++ src/main/scala/CafeXSystem.scala
@@ -3,6 +3,38 @@ trait CafeXSystem {
   def produceStandardBill(purchasedItems: List[Item]): BigDecimal = {
     purchasedItems.map(_.cost).sum
   }
+
+  def produceBillWithServiceCharge(purchasedItems: List[Item]): BigDecimal = {
+    val standardBill = produceStandardBill(purchasedItems)
+    val serviceChargeRate = applyServiceCharge(purchasedItems)
+    val serviceCharge = standardBill * serviceChargeRate
+
+    if (serviceChargeRate == 0.20 && serviceCharge > 20) {
+      standardBill + 20
+    } else {
+      standardBill + serviceCharge
+    }
+  }
+
+  private[this] def applyServiceCharge: PartialFunction[List[Item], BigDecimal] = {
+    case items if allDrinks(items) => 0
+    case items if anyHotFood(items) => 0.20
+    case items if anyFoodItem(items) => 0.10
+  }
+
+  private[this] def allDrinks(items: List[Item]) = {
+    items.forall {
+      item => item.equals(Coffee) || item.equals(Cola)
+    }
+  }
+
+  private[this] def anyFoodItem(items: List[Item]) = {
+    items.contains(CheeseSandwich) || items.contains(SteakSandwich)
+  }
+
+  private[this] def anyHotFood(items: List[Item]) = {
+    items.contains(SteakSandwich)
+  }
 }
 
 object CafeXSystem extends CafeXSystem
--- src/test/scala/CafeXSystemTest.scala
+++ src/test/scala/CafeXSystemTest.scala
@@ -7,46 +7,79 @@ class CafeXSystemTest extends FlatSpec {
     """
       |# CafeX System
       |
-      |##`standardBill`
+      |##`produceStandardBill`
       |It takes a list of items purchased and returns the total bill
       |
+      |##`produceBillWithServiceCharge`
+      |It takes a list of items purchased and returns the total bill with conditional service charge applied
     """.stripMargin
   }
 
-  behavior of "standardBill"
-  it should "calculate the total bill for no items purchased" in {
+  behavior of "produceStandardBill"
+  it should "produce the total bill when no items are purchased" in {
     val purchasedItems = Nil
 
     CafeXSystem.produceStandardBill(purchasedItems) should be(0.0)
   }
 
-  it should "calculate the total bill for items purchased include single Cola" in {
+  it should "produce the total bill when purchased items include single Cola" in {
     val purchasedItems = List(Cola)
 
     CafeXSystem.produceStandardBill(purchasedItems) should be(0.50)
   }
 
-  it should "calculate the total bill for items purchased include single Coffee" in {
+  it should "produce the total bill when purchased items include single Coffee" in {
     val purchasedItems = List(Coffee)
 
     CafeXSystem.produceStandardBill(purchasedItems) should be(1)
   }
 
-  it should "calculate the total bill for items purchased include single Cheese Sandwich" in {
+  it should "produce the total bill when purchased items include single Cheese Sandwich" in {
     val purchasedItems = List(CheeseSandwich)
 
     CafeXSystem.produceStandardBill(purchasedItems) should be(2)
   }
 
-  it should "calculate the total bill for items purchased include single Steak Sandwich" in {
+  it should "produce the total bill when purchased items include single Steak Sandwich" in {
     val purchasedItems = List(SteakSandwich)
 
     CafeXSystem.produceStandardBill(purchasedItems) should be(4.5)
   }
 
-  it should "calculate the total bill for items purchased include single Cola Coffee and Cheese Sandwich" in {
+  it should "produce the total bill when purchased items are single Cola Coffee and Cheese Sandwich" in {
     val purchasedItems = List(Cola, Coffee, CheeseSandwich)
 
     CafeXSystem.produceStandardBill(purchasedItems) should be(3.5)
+  }
+
+  behavior of "applyServiceCharge"
+  it should "apply no service charge when all purchased items are drinks" in {
+    val purchasedItems = List(Cola, Coffee)
+
+    CafeXSystem.produceBillWithServiceCharge(purchasedItems) should be(1.5)
+  }
+
+  it should "apply a service charge of 10% to the total bill when purchased items include any food item" in {
+    val purchasedItems = List(Cola, CheeseSandwich)
+
+    CafeXSystem.produceBillWithServiceCharge(purchasedItems) should be(2.75)
+  }
+
+  it should "apply a service charge of 10% to the total bill rounded to 2 decimal places when purchased items include any food item" in {
+    val purchasedItems = List(Cola, CheeseSandwich, CheeseSandwich, CheeseSandwich)
+
+    CafeXSystem.produceBillWithServiceCharge(purchasedItems) should be(7.15)
+  }
+
+  it should "apply a service charge of 20% to the total bill when purchased items include any hot food" in {
+    val purchasedItems = List(Cola, SteakSandwich)
+
+    CafeXSystem.produceBillWithServiceCharge(purchasedItems) should be(6)
+  }
+
+  it should "apply a service charge of 20% to the total bill when purchased items include any hot food with a maximum of £20 service charge" in {
+    val purchasedItems = List.fill(25)(SteakSandwich)
+
+    CafeXSystem.produceBillWithServiceCharge(purchasedItems) should be(132.5)
   }
 }
